95007f6a5cd58ae262118f0540a178a00a40d8b2
isnaaanr <isnanisa8899@gmail.com>
2023-09-05 12:02:25 +0700

    update

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ $db = new dbController();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
-    </script>
+   
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="style.css">
     <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=62969761faada80019c4386c&product=inline-share-buttons' async='async'></script>
@@ -85,11 +84,6 @@ $db = new dbController();
 
         <!-- Card kelas -->
     <div class="container" id="Kelas">
-        <button title="Back To Top" class="btnScrollToTop bg-success">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" />
-            </svg>
-        </button>
         <h1 class="text-center text-center mb-5 mt-70">Kelas</h1>
         <div class="row text-center justify-content-center">
 
@@ -147,71 +141,74 @@ $db = new dbController();
                 endforeach
                 ?>
             </div>
-            <!-- akhir card guru -->
-
-            <!-- card siswa -->
-            <div class="container" id="Siswa">
-                <h1 class="text-center text-center mb-5 mt-3">Siswa</h1>
-                <div class="row text-center justify-content-center">
-
-                    <?php
-                    // Menggunakan PDO untuk menghubungkan ke database
-                    $db = new PDO("mysql:host=localhost;dbname=sekolah", "root", "");
-
-                    // Kueri SQL dengan JOIN untuk mengambil data siswa, nama kelas, dan nama jurusan
-                    $sql = "SELECT t_siswa.f_nama, t_kelas.f_nama AS f_idkelas, t_jurusan.f_nama AS f_idjurusan
-                        FROM t_siswa
-                        INNER JOIN t_kelas ON t_siswa.f_idkelas = t_kelas.f_idkelas
-                        INNER JOIN t_jurusan ON t_siswa.f_idjurusan = t_jurusan.f_idjurusan";
-
-                    $result = $db->query($sql);
-
-                    foreach ($result as $row) :
-                    ?>
-
-                        <div class="card-all col-md-4 col-xl-6 mb-5" data-aos="zoom-in-down" data-aos-duration="1000">
-                            <div class="card" style="width: 18rem;">
-                                <img src="img/<?php echo $row['f_nama']; ?>.jpeg" class="card-img-top" alt="Siswa">
-                                <div class="card-body">
-                                    <h5 class="card-title text-dark">
-                                        <?php echo $row['f_nama']; ?>
-                                    </h5>
-                                    <span>Kelas: <?php echo $row['f_idkelas']; ?></span>
-                                    <p>Jurusan: <?php echo $row['f_idjurusan']; ?></p>
-                                </div>
-                            </div>
-                        </div>
-
-                    <?php
-                    endforeach;
-                    ?>
-                </div>
-            </div>
         </div>
     </div>
-    <!-- akhir card siswa -->
+            <!-- akhir card guru -->
 
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fill-opacity="1" d="M0,160L34.3,160C68.6,160,137,160,206,165.3C274.3,171,343,181,411,192C480,203,549,213,617,218.7C685.7,224,754,224,823,213.3C891.4,203,960,181,1029,154.7C1097.1,128,1166,96,1234,85.3C1302.9,75,1371,85,1406,90.7L1440,96L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
-    <footer class="text-white text-center mt-25" style= "background: #273036;">
-        <p>
+<!-- tabel siswa -->
+<div class="container" id="Siswa">
+  <div class="text-center text-center mb-5 mt-3"><h1>Siswa</h1>
+  <div class="row justify-content-center">
+  <div class="col-8">
+  <div class="table table-primary table table-success table-striped">
+
+<table class="table">
+  <head>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Kelas</th>
+      <th scope="col">Jurusan</th>
+    </tr>
+  </head>
+     </body>
+
+  <?php
+  $i=1;
+  $sql = "SELECT f_nama, f_idkelas, f_idjurusan FROM t_siswa";
+
+  $row = $db->getALL($sql);
+  foreach ($row as $siswa) :
+  ?>
+
+    <tr>
+      <th scope="row">
+      <?php echo $i++; ?>
+      </th>
+      <td>
+
+        <?php echo $siswa['f_nama']; ?>
+
+      </td>
+      <td><?php echo $siswa['f_idkelas']; ?></td>
+      <td><?php echo $siswa['f_idjurusan']; ?></td>
+    </tr>
+
+    <?php endforeach ?>
+  </body>
+</table>
+  </div>
+  </div>
+  </div>
+</div>
+  </div>
+    <!-- akhir tabel siswa -->
+
+
+    <footer class="text-white text-center mt-25">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00000" fill-opacity="1" d="M0,160L40,154.7C80,149,160,139,240,149.3C320,160,400,192,480,213.3C560,235,640,245,720,234.7C800,224,880,192,960,186.7C1040,181,1120,203,1200,181.3C1280,160,1360,96,1400,64L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+        <p class="bg-black">
         &copy;CopyRight Created by <a href="https://instagram.com/isnansrsy?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D" target="_blank" class="text-white text-decoration-none fw-bold">Isnanisa</a>
         </p>
         </footer>
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script>
-        const btnScrollToTop = document.querySelector(".btnScrollToTop");
 
-        btnScrollToTop.addEventListener("click", function() {
-            window.scrollTo(0, 0);
-        });
-        window.addEventListener("scroll", () => {
-            if (window.pageYOffset > 100) {
-                btnScrollToTop.classList.add("active");
-            } else {
-                btnScrollToTop.classList.remove("active");
-            }
-        })
-        AOS.init();
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+
 </body>
-</html
+</html>
